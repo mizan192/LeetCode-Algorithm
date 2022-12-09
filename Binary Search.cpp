bea3b2cd6b76a6778d60dc3773e145f48b645a74
mizan192 <miz1998an@gmail.com>
2022-12-09 06:24:17 +0600

    more-efficient

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+
+        int left=0,right=nums.size()-1;
+
+        int index=-1;
+
+        while(left<=right){  
+            int mid = left+(right-left)/2;    //no overflow issue
+            if(nums[mid]==target) {
+                index=mid;
+                break;
+            }
+            else if(nums[mid]<target) left=mid+1;
+            else right =mid-1;
+        }
+
+    return index;
+
+    }
+};
+
+/*
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
         int left=-1,right=nums.size()-1;
         while(left<right){  
             int mid = left+(right-left)/2;   //no overflow issues
@@ -13,3 +37,4 @@ public:
 
     }
 };
+*/
